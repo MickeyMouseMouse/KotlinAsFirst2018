7548eb90220a20a205165234fc6659c1cd5f123c
Андрей Джеус <xboooox123@gmail.com>
2018-12-10 21:11:56 +0300

    Lesson 9

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -38,31 +38,47 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E) =
+    if (height > 0 && width > 0)
+        MatrixImpl(height, width, e)
+    else
+        throw IllegalArgumentException()
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E>
+{
+    val matrix = MutableList(height * width){e}
 
-    override val width: Int = TODO()
+    override fun get(row: Int, column: Int) =
+        matrix[row * width + column]
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    override fun get(cell: Cell) =
+        get(cell.row, cell.column)
 
-    override fun get(cell: Cell): E  = TODO()
-
-    override fun set(row: Int, column: Int, value: E) {
-        TODO()
+    override fun set(row: Int, column: Int, value: E)
+    {
+        matrix[row * width + column] = value
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun set(cell: Cell, value: E)
+    {
+        set(cell.row, cell.column, value)
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) : Boolean
+    {
+        if (other !is Matrix<*>) return false
+
+        for (row in 0 until height)
+            for (column in 0 until width)
+                if (get(row, column) != other[row, column]) return false
+
+        return true
+    }
 
     override fun toString(): String = TODO()
 }
